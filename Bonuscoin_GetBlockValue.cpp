eaf25a1bff18f09392d0d34ce56c2cd306f12bfe
miner77 <miner77@users.noreply.github.com>
2014-04-21 01:16:37 +0200

    Update Bonuscoin_GetBlockValue.cpp

--- Bonuscoin_GetBlockValue.cpp
+++ Bonuscoin_GetBlockValue.cpp
@@ -1,11 +1,3 @@
-#include "checkpoints.h"
-#include "db.h"
-#include "txdb.h"
-#include "net.h"
-#include "init.h"
-#include "ui_interface.h"
-#include "checkqueue.h"
-#include "chainparams.h"
 #include <boost/algorithm/string/replace.hpp>
 #include <boost/filesystem.hpp>
 #include <boost/filesystem/fstream.hpp>
