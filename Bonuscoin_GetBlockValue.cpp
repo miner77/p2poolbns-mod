027cbc3d0017d42acd70e5e927ee6d4f43ffccd3
miner77 <miner77@users.noreply.github.com>
2014-04-21 14:42:10 +0200

    Update Bonuscoin_GetBlockValue.cpp

--- Bonuscoin_GetBlockValue.cpp
+++ Bonuscoin_GetBlockValue.cpp
@@ -1,9 +1,24 @@
-#include <stdlib.h>
-#include <stdint.h>
-#include <string.h>
-#include <stdio.h>
-#include <math.h>
-#include <pyconfig.h>
+#include "main.h"
+#include "addrman.h"
+#include "alert.h"
+#include "chainparams.h"
+#include "checkpoints.h"
+#include "checkqueue.h"
+#include "init.h"
+#include "net.h"
+#include "txdb.h"
+#include "txmempool.h"
+#include "ui_interface.h"
+#include "util.h"
+#include <sstream>
+#include <boost/algorithm/string/replace.hpp>
+#include <boost/filesystem.hpp>
+#include <boost/filesystem/fstream.hpp>
+
+using namespace std;
+using namespace boost;
+
+
 
 int static generateMTRandom(unsigned int s, int range)
 {
@@ -12,11 +27,10 @@ int static generateMTRandom(unsigned int s, int range)
     return dist(gen);
 }
 
-static const int64_t COIN = 100000000;
-static const int64_t nStartSubsidy = 2300 * COIN;
-static const int64_t nMinSubsidy = 10 * COIN;
+static const int64 nStartSubsidy = 2300 * COIN;
+static const int64 nMinSubsidy = 10 * COIN;
 
-int64_t static GetBlockValue(int nHeight, int64_t nFees, uint256 prevHash)
+int64 static GetBlockValue(int nHeight, int64 nFees, uint256 prevHash)
 {
 
     std::string cseed_str = prevHash.ToString().substr(25,7);
@@ -25,7 +39,7 @@ int64_t static GetBlockValue(int nHeight, int64_t nFees, uint256 prevHash)
     int rand = generateMTRandom(seed, 1024);
     if (fDebug) { printf(">> nHeight = %d, rand = %d\n", nHeight, rand); }
 
-    int64_t nSubsidy = nStartSubsidy;
+    int64 nSubsidy = nStartSubsidy;
 
     //see if there's a bonus
     //a range of 256 possible values; (256 / 1024) = 1:4
@@ -53,7 +67,7 @@ int64_t static GetBlockValue(int nHeight, int64_t nFees, uint256 prevHash)
         nSubsidy *= 1.25; //bump up the first 100,000 blocks
     } else {
         // Mining phase: Subsidy is cut in half every SubsidyHalvingInterval
-        nSubsidy >>= (nHeight / 300000 );
+        nSubsidy >>= (nHeight / Params().SubsidyHalvingInterval());
     }
 
     // Inflation phase: Subsidy reaches minimum subsidy
@@ -67,6 +81,7 @@ int64_t static GetBlockValue(int nHeight, int64_t nFees, uint256 prevHash)
     //commented out fees, I think this should do what we want
     return nSubsidy; //+ nFees;
 }
+
 #include <boost/python/module.hpp>
 #include <boost/python/def.hpp>
 using namespace boost::python;
